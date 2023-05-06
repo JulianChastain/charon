05c5753fc5b3d406f9965a11263b5d37de859e12
JulianChastain <julian.g.chastain@gmail.com>
2023-05-06 11:55:51 -0400

    barely working typescript example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Chessboard} from 'react-chessboard';
+import {Chess, Square} from 'chess.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+function App(): JSX.Element {
+    const [game, setGame] = useState(new Chess());
+    const [fen, setFen] = useState('start');
+    const [feedback, setFeedback] = useState('White to move');
+    function FeedbackBox({children}: { children: string }): JSX.Element {
+        return (
+            <div
+                style={{
+                    backgroundColor: '#3b4252',
+                    borderColor: '#d8dee9',
+                    borderWidth: '1px',
+                    borderStyle: 'solid',
+                    padding: '16px',
+                    margin: '16px',
+                    color: '#d8dee9',
+                    position: 'absolute',
+                    top: '50%',
+                }}
+            >
+                {children}
+            </div>
+        );
+    }
+    const handleMove = (startSquare: Square, endSquare: Square): boolean => {
+        try {
+            const moveResult= game.move({from: startSquare, to: endSquare})
+            if (moveResult === null) {
+                setFeedback('Invalid move.');
+                return false
+            } else {
+                setFen(game.fen());
+                setFeedback('Correct move!');
+                return true
+            }
+        } catch (err) {
+            setFeedback('That move is not legal!');
+            return false
+        }
+    };
+    return (
+        <div
+            style={{
+                height: '100vh',
+                width: '100vh',
+                }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Chessboard
+                position={fen}
+                onPieceDrop={handleMove}/>
+            <div style={{backgroundColor: '#2e3440', height: '100vh'}}>
+                <FeedbackBox>{feedback}</FeedbackBox>
+            </div>
+        </div>
+    );
 }
 
 export default App;
